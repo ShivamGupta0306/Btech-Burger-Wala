d58d83333c9bdcb6ccd76a7336edfeaacc3f23da
ShivamGupta <shivamgupta86041@gmail.com>
2024-04-30 13:36:05 +0530

    update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,4 @@
-import ErrorHandler from "../utils/ErrorHandler";
+import ErrorHandler from "../utils/ErrorHandler.js";
 
 export const isAuthenticated = (req, res, next) => {
   const token = req.cookies["connect.sid"];
